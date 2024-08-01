c5c687df1ca2cedcd1b1a8767cee65985a9dba12
orch1809 <orch@shufersal.co.il>
2024-08-01 14:53:53 +0300

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 
 from snowflake.snowpark.functions import col
+import requests
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -47,6 +48,9 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string+= fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        # st.text(fruityvice_response.json())
+        fv_dg=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -62,7 +66,7 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered!'+name_on_order, icon="✅")
         
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response.json())
 fv_dg=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
