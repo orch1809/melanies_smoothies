cd1f61a74844f6f8212a7005dc61e287ced7049a
orch1809 <orch@shufersal.co.il>
2024-08-01 14:57:38 +0300

    j

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,3 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered!'+name_on_order, icon="✅")
         
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_dg=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
